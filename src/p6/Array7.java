df0410e4ac7e0b1a5e850f5b6145df1cd2ce7dc6
Nedim Kanat <nedim.kanat1999@gmail.com>
2018-12-15 15:07:27 +0100

    init

---
+++ src/p6/Array7.java
@@ -0,0 +1,85 @@
+package p6;
+
+public class Array7 {
+
+	private int[] elements;
+	
+	public Array7() {
+		
+		// give this.element an array with 7 0s
+		this.elements = new int[7];
+	}
+	
+	/** 
+	 	@param array - an array including 7 elements.<br>
+	 */
+	public Array7(int[] array) {
+		setArray(array);
+	}
+	
+	/**
+	 	Create an array using given Array7 object.
+	 	
+	 	@param array - an Array7 object whose elements will be used.
+	 */
+	public Array7(Array7 array) {
+		setArray(array);
+	}
+	
+	/**
+	 
+	 	@param pos - array element position
+	 	@param value - what value should be used at given position in array
+	  
+	 **/
+	public void setElement(int pos, int value) {
+		this.elements[pos] = value;
+	}
+	
+	/**
+	 	replace current <code>array</code> with given new <code>array</code>
+ 		@param array - a collection of elements.<br> E.g: new int[]{ 1,2,3,4,5,6,7 }
+	 */
+	public void setArray(int[] array) {
+		
+		this.elements = array;
+	}
+	
+	/**
+		@param array - An Array7 object whose elements will be used
+	 */
+	public void setArray(Array7 array) {
+		
+		// Replace elements from given Array7 Object with the elements of Current Objects.
+		for(int i = 0; i < array.elements.length; i++) 
+		{
+			this.elements[i] = array.elements[i];
+		}
+	}
+	
+	/**
+	 	@returns Array7 object
+	 **/
+	public Array7 getArray() {
+		return new Array7(this.elements);
+	}
+	
+	
+	/**
+	  	@returns Array7 object as int[]
+	 **/
+	public int[] toIntArray() {
+		return this.elements;
+	}
+	
+	
+	/**
+	  	@param pos - position of an element
+	 **/
+	public int getElement(int pos) {
+		return this.elements[pos];
+	}
+	
+	
+	
+}
---
+++ src/p6/Array7x7.java
@@ -0,0 +1,78 @@
+package p6;
+
+/*
+ Array7x7 
+ 			row, 	col, 	value
+ setElement(4, 		2, 		432)
+ 
+ elements = 
+ {
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0},		0
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0},		1
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0},		2
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0},		3
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0},		4
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0},		5
+ 	{0, 0, 	0, 	0, 	0, 	0, 	0}		6
+ 	
+ 	0	1	2	3	4	5	6
+ }
+  
+  
+ */
+
+public class Array7x7 {
+	
+	private int[][] elements;
+	
+	public Array7x7() {
+		elements = new int[7][7];
+	}
+	
+	
+	public void setElement(int row, int col, int value) {
+		this.elements[row][col] = value;
+	}
+	
+	public int getElement(int row, int col) {
+		return this.elements[row][col];
+	}
+	
+	public void setRow(int row, Array7 theRow) {
+		this.elements[row] = theRow.toIntArray(); 
+	}
+	
+	public Array7 getRow(int row) {
+		return new Array7(this.elements[row]);
+	}
+	
+	public void setCol(int col, Array7 theCol) {
+		for(int i = 0; i < theCol.toIntArray().length; i++) {
+			this.elements[col][i] = theCol.getElement(i);
+		}
+	}
+	
+	public Array7 getCol(int col) {
+		Array7 arr = new Array7();
+		for(int i = 0; i < this.elements[0].length; i++) {
+			arr.setElement(this.elements[col][i], i);
+		}
+		return arr;
+	}
+	
+	public void setArray(Array7x7 array7x7) {
+		this.elements = array7x7.toIntArray();
+	}
+	public void setArray(int[][] array) {
+		this.elements = array;
+	}
+	
+	public Array7x7 getArray() {
+		return this;
+	}
+	
+	public int[][] toIntArray(){
+		return this.elements;
+	}
+	
+}
